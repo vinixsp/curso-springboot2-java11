9f2f8124ce727b08415ae0a297817f64ce81494e
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-13 17:59:23 -0300

    Camada de serviço, registro de

--- src/main/java/com/varod/course/resources/UserResource.java
+++ src/main/java/com/varod/course/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.varod.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.varod.course.entities.User;
+import com.varod.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Vinicius Rodrigues", "vinicius.rodrigues@gmail.com", "11 97236-8592", "123456");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User u = service.findById(id);
 		return ResponseEntity.ok().body(u);
 	}
-
 }

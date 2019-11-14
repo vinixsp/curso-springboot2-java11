dd1dc79af798844b30cefdb29444384e892a003f
Vinicius Rodrigues <vinicius.rodrigues@gmail.com>
2019-11-13 23:38:45 -0300

    OrderStatus enum

--- src/main/java/com/varod/course/entities/Order.java
+++ src/main/java/com/varod/course/entities/Order.java
@@ -12,30 +12,34 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.varod.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_orders")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant instant;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
 	}
 
-	public Order(Long id, Instant instant, User client) {
+	public Order(Long id, Instant instant, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.instant = instant;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -53,6 +57,16 @@ public class Order implements Serializable {
 
 	public void setInstant(Instant instant) {
 		this.instant = instant;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
